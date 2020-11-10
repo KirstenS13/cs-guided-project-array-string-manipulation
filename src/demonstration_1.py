94c686511e0697e12a8249c8b6a7c3fccb4a0f1f
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-11-09 19:45:34 -0500

    finished lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -23,3 +23,52 @@ There is no index that satisfies the conditions in the problem statement.
 def pivot_index(nums):
     # Your code here
 
+    # UNDERSTAND
+    # don't include the actual index in either of the sums
+    # lets assume that a single number would return -1
+
+    # PLAN
+    # default return variable is -1
+    # if nums is empty or size 1, return -1
+
+    # Approach 1 - runtime O(n^2)
+    # The most straight forward:
+    # iterate through
+    # for i in range(len(nums)):
+    #     # for each index, sum up the left side and the right side and compare
+    #     left_sum = sum(nums[0:1])
+    #     right_sum = sum(nums[i+1])
+
+    # Approach 2
+    # left_side_sum = 0
+    # iterate through the array
+    #   add nums[i] to left_side_sum
+    #   iterate through the array from the end and work back towards i
+    #       add to right_side_sum
+    # 
+    # this is still O(n^2) because we were recalculating the right side sum
+    # how / where do we calculate the right_side_sum?
+
+    # Approach 3
+    if len(nums) <= 1:
+        return -1
+    # left = 0
+    left = 0
+    # right = sum of the entire array
+    right = sum(nums)
+    # iterate through the array
+    # we want the index, so use range
+    for i in range(len(nums)):
+        #   right side = sum of the entire array - left - nums[i]
+        right -= nums[i] # subtract the value at i from right first, so that right doesn't include the value at index
+        # compare, and if they are the same, return i
+        if left == right:
+            return i
+    #   add nums[i] to the left_side_sum
+        left += nums[i]
+
+    return -1
+
+print(pivot_index([1,7,3,6,5,6]))
+
+    
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -26,5 +26,44 @@ Output: [1,0,0,0]
 Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
 def plus_one(digits):
-    # Your code here
+#     # Your code here
+#     for i in range(len(digits), 0, -1):
+#         print(i)
 
+# print(plus_one([1, 2, 3]))
+
+    # UNDERSTAND
+    # add one to the last number if it's not a 9
+    # if it is a 9, make it a 0, and then repeat with the second to last numbre
+    # if the number is a 9:
+    #   make it 0, and repeat with previous number
+    # else if it's not a 9:
+    #   add 1
+    # if we get the the first number and it's a 9, make it a 10
+
+
+    # PLAN
+    # index = len(digits)
+    # loop until: the current number is not 9
+    #   if the number is a 9:
+    #       make it zero, and repeat w/ previous number
+    #       set digits[i] to 9
+    #   else if it's not a 9:
+    #       add 1 to digits[i]
+    #   decrement index
+    # if we get to the first number (index == 0) and it's a 9, make it a 10 (insert 1 at the 0th index, make 1st index 0)
+
+    index = len(digits) - 1
+    while index >= 0 and digits[index] == 9:
+        digits[index] = 0
+        index = index - 1
+
+    if index == -1:
+        digits.insert(0, 1)
+    else:
+        digits[index] += 1
+
+    return digits
+
+
+print(plus_one([1, 2, 3]))
